4e88b25c380e372cca8bbc37786a724f45b49189
Lefort <maxime.lefort3@orange.fr>
2018-03-17 04:45:07 +0100

    js resolved

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
 /* Show and hide menu */
-$(document).ready(function)(){
+$(document).ready(function(){
 	
 	'use strict';
 	
 	
-	$(window).scroll(function(){
+$(window).scroll(function(){
 		
 		
 		'use strict';
@@ -53,7 +53,7 @@ $(document).ready(function)(){
 	});
 	
 //add smooth scrolling
-$(document).ready(function() {
+$(document).ready(function(){
 	
 	'use strict';
 	
@@ -61,13 +61,13 @@ $(document).ready(function() {
 	  $('.nav-item, #scroll-to-top')
   
  
-    .click(function()) {
+    .click(function() {
     
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
       && 
       location.hostname == this.hostname
-    ) {
+      ){
       
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -90,3 +90,116 @@ $(document).ready(function() {
 	
 
 });
+/* active menu on click */
+$(document).ready(function(){
+
+     'use strict';
+	
+	
+	 $('.navbar-nav li a').click(function(){
+	
+	   'use strict';	
+	
+         $('.navbar-nav li a').parent().removeClass("active")
+		 
+		 $(this).parent().addClass("active");
+		 
+		 
+	 });
+
+
+});
+// highlight menu item on scroll
+$(document).ready(function(){
+				  
+		'use strict';		  
+				  
+				  
+			$(window).scroll(function(){
+				
+				'use strict';
+				
+				$("section").each(function() {
+					
+					
+					'use strict';
+					
+					var bb = $(this).attr("id"); // ABOUT, CONTACT, DOWNLOAD 
+					var hei = $(this).outerHeight();
+					var grttop = $(this).offset().top - 70;
+					
+					if ($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei) {
+						
+						$(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+						
+						
+						
+					} else {
+						
+						$(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+						
+						
+						
+					}
+					
+				});
+				
+				
+				
+		   });	  
+							 
+							 
+							 
+							 
+	 });
+// add auto padding to header
+
+$(document).ready(function(){
+	
+	'use strict';
+	
+	setInterval(function(){
+		
+		'use strict';
+		
+		var windowHeight = $(window).height();
+		
+		var containerHeight = $(".header-container").height();
+		
+		var padTop = windowHeight - containerHeight;
+		
+		
+		$(".header-container").css({
+			
+			'padding-top': Math.round( padTop / 2)	+ 'px',
+			'padding-bottom' : Math.round( padTop / 2) + 'px'
+			
+			
+		});
+		
+	}, 10)
+	
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
