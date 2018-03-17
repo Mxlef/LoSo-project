6bf93dfd7f55f1c4aaf1782967a0e1a0ea011d5d
Lefort <maxime.lefort3@orange.fr>
2018-03-17 01:58:01 +0100

    navbar

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+/* Show and hide menu */
+$(document).ready(function(){
+	
+	'use strict';
+	
+	
+	$(window).scroll(function() {
+		
+		
+		'use strict';
+		
+		if($(window).scrollTop() < 80 ) {
+			
+			$('.navbar').css ({
+				
+				'margin-top': '-100px',
+				'opacity': '0'
+				
+				
+			});
+			
+			
+		} else {
+		   $('.navbar').css ({
+				
+				'margin-top': '-100px',
+				'opacity': '1' 
+		
+		
+	    });
+	
+	
+	  }
+	
+	
+	});
+	
+});
